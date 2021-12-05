b85669e1924a44be43e8746a077bd8ad9cbaaa73
CosmoMatt <matt.price@kagenova.com>
2021-12-05 20:06:08 +0000

    add basic tests to increase coverage

--- optimusprimal/tests/test_proxs.py
+++ optimusprimal/tests/test_proxs.py
@@ -1,4 +1,5 @@
 import optimusprimal.prox_operators as prox_operators
+import optimusprimal.linear_operators as linear_operators
 import pytest
 
 import numpy as np
@@ -21,6 +22,35 @@ def test_l2_ball_op():
     assert op.fun(inp) >= 0
 
 
+def test_l2_ball_errors():
+    with pytest.raises(ValueError):
+        prox_operators.l2_ball(epsilon=0, data=None)
+
+
+def test_l_inf_ball_op():
+    epsilon = 2.0
+    inp = np.random.normal(0, 10.0, (10, 10))
+    out = np.zeros_like(inp)
+    out = np.minimum(epsilon, np.abs(out)) * np.exp(complex(0, 1) * np.angle(out)) + inp
+    op = prox_operators.l_inf_ball(epsilon, inp)
+    assert op.fun(inp) >= 0
+    assert np.allclose(op.prox(inp, 0.0), out, 1e-6)
+
+    op = prox_operators.l_inf_ball(epsilon, inp, Phi=linear_operators.identity())
+    assert op.fun(inp) >= 0
+    assert np.allclose(op.prox(inp, 0.0), out, 1e-6)
+
+    x = op.dir_op(inp)
+    xx = op.adj_op(inp)
+    assert np.allclose(x, xx, 1e-10)
+    assert np.allclose(x, inp, 1e-10)
+
+
+def test_l_inf_ball_errors():
+    with pytest.raises(ValueError):
+        prox_operators.l_inf_ball(epsilon=0, data=None)
+
+
 def test_l1_norm_op():
     gamma = 2
     inp = np.random.normal(0, 10.0, (10, 10))
@@ -35,3 +65,115 @@ def test_l1_norm_op():
     op = prox_operators.l1_norm(gamma)
     assert op.fun(inp) >= 0
     assert np.allclose(op.prox(inp, 1), out, 1e-6)
+
+
+def test_l1_norm_op_errors():
+    with pytest.raises(ValueError):
+        prox_operators.l1_norm(gamma=0)
+
+
+def test_l2_square_norm_op():
+    sigma = tau = 2
+    inp = np.random.normal(0, 10.0, (10, 10))
+    out = inp / (tau / sigma ** 2 + 1.0)
+    op = prox_operators.l2_square_norm(sigma)
+    assert op.fun(inp) == pytest.approx(np.sum(np.abs(inp) ** 2 / (2.0 * sigma ** 2)))
+    assert np.allclose(op.prox(inp, tau), out, 1e-6)
+
+    op = prox_operators.l2_square_norm(sigma, Psi=linear_operators.identity())
+    assert op.fun(inp) == pytest.approx(np.sum(np.abs(inp) ** 2 / (2.0 * sigma ** 2)))
+    assert np.allclose(op.prox(inp, tau), out, 1e-6)
+
+    x = op.dir_op(inp)
+    xx = op.adj_op(inp)
+    assert np.allclose(x, xx, 1e-10)
+    assert np.allclose(x, inp, 1e-10)
+
+
+def test_l2_square_norm_op_errors():
+    with pytest.raises(ValueError):
+        prox_operators.l2_square_norm(sigma=0)
+
+
+def test_positive_prox_op():
+    inp = np.random.normal(0, 10.0, (10, 10))
+    out = np.zeros_like(inp)
+    out[inp >= 0] = inp[inp >= 0]
+    op = prox_operators.positive_prox()
+    assert op.fun(inp) == pytest.approx(0.0)
+    assert np.allclose(op.prox(inp, 1), out, 1e-6)
+
+    x = op.dir_op(inp)
+    xx = op.adj_op(inp)
+    assert np.allclose(x, xx, 1e-10)
+    assert np.allclose(x, inp, 1e-10)
+
+
+def test_real_prox_op():
+    inp = np.random.normal(0, 10.0, (10, 10)) + 1j * np.random.normal(0, 10.0, (10, 10))
+    out = np.real(inp)
+    op = prox_operators.real_prox()
+    assert op.fun(inp) == pytest.approx(0.0)
+    assert np.allclose(op.prox(inp, 1), out, 1e-6)
+
+    x = op.dir_op(inp)
+    xx = op.adj_op(inp)
+    assert np.allclose(x, xx, 1e-10)
+    assert np.allclose(x, inp, 1e-10)
+
+
+def test_zero_prox_op():
+    inp = np.random.normal(0, 10.0, (10, 10))
+    indices = np.where(inp > 0)
+    out = np.copy(inp)
+    out[out > 0] = 0.0
+    op = prox_operators.zero_prox(
+        indices=indices, op=linear_operators.identity(), offset=0
+    )
+    assert op.fun(inp) == pytest.approx(0.0)
+    assert np.allclose(op.prox(inp, 1), out, 1e-6)
+
+    x = op.dir_op(inp)
+    xx = op.adj_op(inp)
+    assert np.allclose(x, xx, 1e-10)
+    assert np.allclose(x, inp, 1e-10)
+
+
+def test_l21_norm():
+    tau = 1.0
+    l2axis = 0
+    inp = np.random.normal(0, 10.0, (10, 10))
+    out = np.expand_dims(np.sqrt(np.sum(np.square(np.abs(inp)), axis=l2axis)), l2axis)
+    out = inp * (1 - tau / np.maximum(out, tau))
+    op = prox_operators.l21_norm(tau, l2axis)
+    assert op.fun(inp) == pytest.approx(0.0)
+    assert np.allclose(op.prox(inp, tau), out, 1e-6)
+
+    op = prox_operators.l21_norm(tau, l2axis, Phi=linear_operators.identity())
+    assert op.fun(inp) == pytest.approx(0.0)
+    assert np.allclose(op.prox(inp, tau), out, 1e-6)
+
+    x = op.dir_op(inp)
+    xx = op.adj_op(inp)
+    assert np.allclose(x, xx, 1e-10)
+    assert np.allclose(x, inp, 1e-10)
+
+
+def test_l21_norm_errors():
+    with pytest.raises(ValueError):
+        prox_operators.l21_norm(tau=0)
+
+
+def test_translate_prox():
+    inp = np.random.normal(0, 10.0, (10, 10))
+
+    out = np.maximum(0, 2 * np.real(inp)) - inp
+
+    op = prox_operators.translate_prox(prox_operators.positive_prox(), inp)
+    assert op.fun(inp) == pytest.approx(0.0)
+    assert np.allclose(op.prox(inp, 1), out, 1e-6)
+
+    x = op.dir_op(inp)
+    xx = op.adj_op(inp)
+    assert np.allclose(x, xx, 1e-10)
+    assert np.allclose(x, inp, 1e-10)
--- optimusprimal/tests/test_uq.py
+++ optimusprimal/tests/test_uq.py
@@ -3,7 +3,7 @@ import pytest
 import numpy as np
 
 
-def test_bisection():
+def test_bisection_1D_2D():
     def obj(x):
         return np.sum(x ** 2) - 9
 
@@ -49,3 +49,61 @@ def test_bisection():
     assert np.allclose(error_m.shape, (128 / region_size,), 1e-3)
     assert np.allclose(error_p, 2.0 / np.sqrt(region_size), 1e-3)
     assert np.allclose(error_m, -2.0 / np.sqrt(region_size), 1e-3)
+
+
+def test_bisection_fast_1D_2D():
+    import optimusprimal.linear_operators as lin_ops
+
+    psi = lin_ops.identity()
+    phi = lin_ops.identity()
+
+    x_sol = np.zeros((128, 128))
+    region_size = 16
+
+    def map_loss(x):
+        return np.sum(2 * np.abs(x) ** 2)
+
+    def map_loss_fast(x, xm, w, wm):
+        return (
+            np.sum(np.abs(x) ** 2)
+            + np.sum(np.abs(xm) ** 2)
+            + np.sum(np.abs(w) ** 2)
+            + np.sum(np.abs(wm) ** 2)
+        )
+
+    bound = 4.0
+    iters = 1e5
+    tol = 1e-12
+
+    error_p, error_m, mean = map_uncertainty.create_local_credible_interval(
+        x_sol, region_size, map_loss, bound, iters, tol, -5, 5
+    )
+
+    (
+        error_p_fast,
+        error_m_fast,
+        mean_fast,
+    ) = map_uncertainty.create_local_credible_interval_fast(
+        x_sol, phi, psi, region_size, map_loss_fast, bound, iters, tol, -5, 5
+    )
+    assert np.allclose(error_p, error_p_fast, 1e-3)
+    assert np.allclose(error_m, error_m_fast, 1e-3)
+    assert np.allclose(mean, mean_fast, 1e-12)
+
+    x_sol = np.zeros((128,))
+    region_size = 16
+
+    error_p, error_m, mean = map_uncertainty.create_local_credible_interval(
+        x_sol, region_size, map_loss, bound, iters, tol, -5, 5
+    )
+
+    (
+        error_p_fast,
+        error_m_fast,
+        mean_fast,
+    ) = map_uncertainty.create_local_credible_interval_fast(
+        x_sol, phi, psi, region_size, map_loss, bound, iters, tol, -5, 5
+    )
+    assert np.allclose(error_p, error_p_fast, 1e-3)
+    assert np.allclose(error_m, error_m_fast, 1e-3)
+    assert np.allclose(mean, mean_fast, 1e-12)
